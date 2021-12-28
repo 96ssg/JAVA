eb0e6e7b977522aa0f03feb89c33ef791e953227
96ssg <96719741+96ssg@users.noreply.github.com>
2021-12-28 09:15:42 +0900

    Update HelloJava.java

--- hellojava/src/com/yedam/java/main/HelloJava.java
+++ hellojava/src/com/yedam/java/main/HelloJava.java
@@ -6,7 +6,7 @@ public class HelloJava {
 		// 시스템에 출력하는 기능
         System.out.println("Wellcome to JAVA");
         System.out.println("2021-12-24");
-      
+        System.out.println("Today...");
         
 	}
 
