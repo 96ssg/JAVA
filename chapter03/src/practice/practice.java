b5199222502e8163fa5d5b27e0a68c30efde9e2e
admin <admin@YD5-11>
2021-12-29 09:11:29 +0900

    혼공자 chapter 02~04 12-28

---
+++ chapter03/src/practice/practice.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		int b = 5;
+		b = -b;
+		int result = 10 / b;
+		System.out.println(result);
+		
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--);
+		System.out.println(z);
+		
+		int var1 = 5;
+		int var2 = 2;
+		double var3 = var1 / var2;
+		int var4 = (int) (var3 * var2);
+		System.out.println(var4);
+		
+		int value = 356;
+		System.out.println((value/100)*100);
+		
+		
+		
+	}
+
+}
